b925500df71d89b1175b62bf6940c152f67f55db
JasonStaubach <jbstaubach@gmail.com>
2023-07-21 09:51:16 -0400

    improved store index and root index of frontend

---
+++ staubify_frontend/src/store/index.js
@@ -0,0 +1,30 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import songsReducer from './songs';
+import usersReducer from './users';
+import artistsReducer from './artists'
+
+
+export const rootReducer = combineReducers({
+    artists: artistsReducer,
+    songs: songsReducer,
+    users: usersReducer,
+
+});
+
+let enhancer;
+
+if (process.env.NODE_ENV === 'production') {
+  enhancer = applyMiddleware(thunk);
+} else {
+  const logger = require('redux-logger').default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+
+const configureStore = (preloadedState = {}) => {
+    return createStore(rootReducer, preloadedState, enhancer)
+}
+
+export default configureStore
