e5ea69a65a44bc652899adebb1baccada0eaae45
JasonStaubach <jbstaubach@gmail.com>
2023-07-24 15:47:04 -0400

    fix seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-ApplicationRecord.transaction do 
-    puts "Creating users"
-    User.create!(username: 'Demo', password: 'password', birthday: Date.new(2001,4,5), email: "abc@gmail.com")
-end
+# ApplicationRecord.transaction do 
+#     puts "Creating users"
+#     User.create!(username: 'Demo', password: 'password', birthday: Date.new(2001,4,5), email: "abc@gmail.com")
+# end
