38665a676cad5e7c0375a92fc9a2fd2cb2156867
JasonStaubach <jbstaubach@gmail.com>
2023-07-25 13:26:07 -0400

    the margins on the homepage are starting to do something kinda

--- frontend/src/components/Homepage/index.js
+++ frontend/src/components/Homepage/index.js
@@ -1,17 +1,21 @@
 
+import { useEffect } from "react";
 import HeaderBar from "../HeaderBar"
 import SideBar from "../SideBar"
+import { useDispatch, useSelector } from "react-redux";
 
 export default function HomePage(){
+    const dispatch = useDispatch()
 
-
-    function resizer(){
+    const resizer = () => {
+        let leftBarSize = 10
         debugger
-        let leftBarSize = 90
         let element = document.getElementById("home-page-right");
         element.style.width = `${100-leftBarSize}%`;
         element = document.getElementById("side-bar")
         element.style.width = `${leftBarSize}%`;
+        // element = document.getElementsByClassName("header-bar");
+        // element.style.width = `${leftBarSize}%`;
     }
 
     return(
@@ -19,13 +23,12 @@ export default function HomePage(){
             <div id="home-page">
                 <div id="fixed-page">
                     <SideBar/>
-                    <div className="resizer-bar"/>
+                    <div className="resizer-bar" onMouseDown={resizer}/>
                     <div id="home-page-right">
                         <HeaderBar/>
                     </div>
                 </div>
             </div>
-        {resizer()}
         </>
     )
 
