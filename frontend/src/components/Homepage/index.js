da6a09bcca7891d55f9e337561da8364de9d4f8d
JasonStaubach <jbstaubach@gmail.com>
2023-07-25 13:08:52 -0400

    trying to get this code to work plz its so broken

--- frontend/src/components/Homepage/index.js
+++ frontend/src/components/Homepage/index.js
@@ -3,14 +3,30 @@ import HeaderBar from "../HeaderBar"
 import SideBar from "../SideBar"
 
 export default function HomePage(){
+
+
+    function resizer(){
+        debugger
+        let leftBarSize = 90
+        let element = document.getElementById("home-page-right");
+        element.style.width = `${100-leftBarSize}%`;
+        element = document.getElementById("side-bar")
+        element.style.width = `${leftBarSize}%`;
+    }
+
     return(
-        <div id="home-page">
-            <SideBar/>
-            <div className="resizer-bar"/>
-            <div id="home-page-right">
-                <HeaderBar/>
+        <>
+            <div id="home-page">
+                <div id="fixed-page">
+                    <SideBar/>
+                    <div className="resizer-bar"/>
+                    <div id="home-page-right">
+                        <HeaderBar/>
+                    </div>
+                </div>
             </div>
-        </div>
+        {resizer()}
+        </>
     )
 
 }
