02a75283c627130be48bf0220feead2faa155fdc
JasonStaubach <jbstaubach@gmail.com>
2023-07-25 11:42:42 -0400

    setup new new component layout

--- frontend/src/components/Homepage/index.js
+++ frontend/src/components/Homepage/index.js
@@ -1,9 +1,15 @@
-import StickyBar from "../StickyBar"
 
-export default function Homepage(){
+import HeaderBar from "../HeaderBar"
+import SideBar from "../SideBar"
+
+export default function HomePage(){
     return(
         <div id="home-page">
-            <StickyBar/>
+            <SideBar/>
+            <div className="resizer-bar"/>
+            <div id="home-page-right">
+                <HeaderBar/>
+            </div>
         </div>
     )
 
