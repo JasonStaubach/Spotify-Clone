8d2cbf7eb67c0cf37ba7338187000901041ae223
JasonStaubach <jbstaubach@gmail.com>
2023-07-25 15:08:57 -0400

    testing

--- frontend/src/components/Homepage/index.js
+++ frontend/src/components/Homepage/index.js
@@ -1,21 +1,45 @@
 
-import { useEffect } from "react";
-import HeaderBar from "../HeaderBar"
-import SideBar from "../SideBar"
+import { useEffect, useState } from "react";
+// import { Window } from "react-dom";
+import HeaderBar from "../HeaderBar";
+import SideBar from "../SideBar";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function HomePage(){
     const dispatch = useDispatch()
-
+    
+    const [mouseCoordinates, setMouseCoordinates] = useState({x:0, y:0});
+	
+    const mouseMoveHandler = (e) => {
+        setMouseCoordinates({
+            x:e.clientX,
+            y:e.clientY
+    	});
+    }
+    useEffect(()=>{
+        window.addEventListener('mousemove', mouseMoveHandler);
+        resizer()
+    	return(()=>{
+            window.removeEventListener('mousemove', mouseMoveHandler);
+    	})
+    }, [])
+    
     const resizer = () => {
-        let leftBarSize = 10
-        debugger
+        // const window = window
+        let leftBarSize = Math.random()*30 + 20
+        let homepage = document.getElementById("home-page").offsetWidth
+        console.log(`${mouseCoordinates.x} ${mouseCoordinates.y}`)
+        console.log(`${homepage}`)
+        // debugger
         let element = document.getElementById("home-page-right");
-        element.style.width = `${100-leftBarSize}%`;
+        element.style.width = `${homepage - mouseCoordinates.x}px`;
         element = document.getElementById("side-bar")
-        element.style.width = `${leftBarSize}%`;
-        // element = document.getElementsByClassName("header-bar");
-        // element.style.width = `${leftBarSize}%`;
+        element.style.width = `${mouseCoordinates.x}px`;
+        let elements = document.getElementsByClassName("header-bar");
+        let arrEle = [...elements]
+        arrEle.forEach(element => {
+            element.style.width = `${homepage - mouseCoordinates.x}}px`;     
+        });
     }
 
     return(
