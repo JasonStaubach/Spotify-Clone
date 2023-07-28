1fb8a57859fb56c50a03cd5091c3b343172e1c38
JasonStaubach <jbstaubach@gmail.com>
2023-07-28 10:08:23 -0400

    holy shit you can see seals face

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,7 +7,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-ApplicationRecord.transaction do 
+
+# ApplicationRecord.transaction do 
     puts "Creating users"
     User.create!(username: 'Demo', password: 'password', birthday: Date.new(2001,4,5), email: "abc@gmail.com")
     puts "Creating artists"
@@ -14,4 +17,10 @@ ApplicationRecord.transaction do
     Artist.create!(name: "Lynyrd Skynyrd")
     Artist.create!(name: "Hozier")
     Artist.create!(name: "Aretha Franklin")
-end
+    Artist.first(1).each_with_index do |artist, index|
+        artist.photo.attach(
+            io: URI.open("https://staubify-dev.s3.amazonaws.com/artist_#{index + 1}.jpg"),
+            filename: "artist_#{index + 1}.jpg"
+        )
+    end
+# end
